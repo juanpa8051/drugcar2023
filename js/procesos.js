75711746307341de2c660202b6a4ee0b3d557017
juanpa8051 <139301693+juanpa8051@users.noreply.github.com>
2023-09-14 15:09:25 -0500

    Add files via upload

---
+++ js/procesos.js
@@ -0,0 +1,213 @@
+/*PRODUCCTO 1 */
+
+
+function mostrar1() {
+  document.getElementById("info1").innerHTML = "<h3>Whey protein</h3>"
+  +"<h3> codigo: 790011210146</h3>"
+  +"<h3> Peso Total: 2.03 libras (0.92 kg)</</h3>"
+  +"<h3> precio:$ 135.623</h3>"
+  +"<h3> cantidad #: 25 </h3>"
+  +"<h3> presentacion:tarro 32 oz (908 g)</</h3>"
+  +"<h3> Vence: 28 Diciembre 2024</h3>"
+  +"<h3> Origen: EUA </h3>"
+  
+ 
+  
+
+}
+
+/*PRODUCCTO 2 */
+
+function mostrar2() {
+  document.getElementById("info2").innerHTML = "<h3>chanca piedra</h3>"
+  +"<h3> codigo: 838451002614</h3>"
+  +"<h3> Peso Total: 2.03 libras (0.92 kg)</</h3>"
+  +"<h3> precio:$ 59.680</h3>"
+  +"<h3> cantidad #: 20 </h3>"
+  +"<h3> presentacion:1 fl oz (30 ml)</h3>"
+  +"<h3> Vence: 28 Agosto 2027</h3>"
+  +"<h3> Origen: EUA </h3>"
+}
+
+
+/*PRODUCCTO 3 */
+
+function mostrar3() {
+  document.getElementById("info3").innerHTML = "<h3>omega+CQ10</h3>"
+  +"<h3> codigo:710013001092</h3>"
+  +"<h3> Peso Total: 0.6 libras (0.27 kg)</</h3>"
+  +"<h3> precio:$ 75.763</h3>"
+  +"<h3> cantidad #: 10 </h3>"
+  +"<h3> presentacion:100 mg - 30 Cápsulas Liqui-Gel</h3>"
+  +"<h3> Vence: 30 Agosto 2025</h3>"
+  +"<h3> Origen: EUA </h3>"
+
+}
+
+/*PRODUCCTO 4 */
+
+function mostrar4() {
+  document.getElementById("info4").innerHTML = "<h3>Cartílago de tiburón</h3>"
+  +"<h3> codigo:733739432704</h3>"
+  +"<h3> Peso Total: 0.3 libras (0.14 kg)</h3>"
+  +"<h3> precio:$ 54.168</h3>"
+  +"<h3> cantidad #: 15 </h3>"
+  +"<h3> presentacion:750 mg - 100 Cápsulas </h3>"
+  +"<h3> Vence: 12 sep 2028</h3>"
+  +"<h3> Origen: EUA </h3>"
+  
+}
+  /*PRODUCCTO 5 */
+
+  function mostrar5() {
+    document.getElementById("info5").innerHTML = "<h3>CLA Core</h3>"
+    +"<h3> codigo:733739232708</h3>"
+    +"<h3> Peso Total: 0.4 libras (0.16 kg)</h3>"
+    +"<h3> precio:$ $38.000</h3>"
+    +"<h3> cantidad #: 15 </h3>"
+    +"<h3> presentacion: tarro por 90 Cápsulas </h3>"
+    +"<h3> Vence: 18 nov 2026</h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+
+  
+  }
+  
+
+  /*PRODUCCTO 6 */
+
+  function mostrar6() {
+    document.getElementById("info6").innerHTML = "<h3>Ácido hialurónico </h3>"
+    +"<h3> codigo: 076280103687</h3>"
+    +"<h3> Peso Total: 0.09 libras (0.04 kg)</h3>"
+    +"<h3> precio:$ $ 95.551</h3>"
+    +"<h3> cantidad #: 25 </h3>"
+    +"<h3> presentacion: tarro por 30 Cápsulas </h3>"
+    +"<h3> Vence: 18 nov 2026</h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+
+  
+  }
+
+    
+
+  /*PRODUCCTO 7 */
+
+  function mostrar7() {
+    document.getElementById("info7").innerHTML = "<h3> bioderma</h3>"
+    +"<h3> codigo: 070768025058 </h3>"
+    +"<h3> Peso Total: 16.9 oz </h3>"
+    +"<h3> precio:$55.000 </h3>"
+    +"<h3> cantidad #: 10 </h3>"
+    +"<h3> presentacion: frz  liguido 500 ml </h3>"
+    +"<h3> Vence: 2 dic 2024 </h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+
+  
+  }
+  
+
+  /*PRODUCCTO 8 */
+
+  function mostrar8() {
+    document.getElementById("info8").innerHTML = "<h3> vitamina c </h3>"
+    +"<h3> codigo: 030768025052 </h3>"
+    +"<h3> Peso Total: 0.99 libras (0.45 kg)</h3>"
+    +"<h3> precio:$ 106.619 </h3>"
+    +"<h3> cantidad #: 30 </h3>"
+    +"<h3> presentacion: tarro 1,000 mg - 300 Tabletas</h3>"
+    +"<h3> Vence: 15 dic 2026</h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+}
+
+
+  /*PRODUCCTO 9 */
+
+  function mostrar9() {
+    document.getElementById("info9").innerHTML = "<h3>b complex</h3>"
+    +"<h3> codigo: 070768025058 </h3>"
+    +"<h3> Peso Total: : 0.13 libras (0.06 kg) </h3>"
+    +"<h3> precio:$ 20.014 </h3>"
+    +"<h3> cantidad #: 13 </h3>"
+    +"<h3> presentacion: frz  de 60 tabl </h3>"
+    +"<h3> Vence: 28 Agosto 2026 </h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+
+  
+  }
+  
+
+
+
+
+
+  /*PRODUCCTO 10 */
+
+  function mostrar10() {
+    document.getElementById("info10").innerHTML = "<h3> vitamina d3</h3>"
+    +"<h3> codigo: 070768025058 </h3>"
+    +"<h3> Peso Total: 0.12 libras (0.05 kg) </h3>"
+    +"<h3> precio:$ 37.226 </h3>"
+    +"<h3> cantidad #: 23 </h3>"
+    +"<h3> presentacion: 10 mcg (400 IU) - 120 Cápsulas Liqui-Gel</h3>"
+    +"<h3> Vence: 28 Abr 2025 </h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+
+  
+  }
+
+
+  /*PRODUCCTO 11 */
+
+  function mostrar11() {
+    document.getElementById("info11").innerHTML = "<h3> ginkgo biloba</h3>"
+    +"<h3> codigo:310539673300 </h3>"
+    +"<h3> Peso Total: 0.19 libras (0.09 kg) </h3>"
+    +"<h3> precio:	$ 165.034  </h3>"
+    +"<h3> cantidad #: 21 </h3>"
+    +"<h3> presentacion: 120 mg - 60 Tabletes</h3>"
+    +"<h3> Vence: 28 Diciembre 2024 </h3>"
+    +"<h3> Origen: germany </h3>"
+  
+
+  
+  }
+
+
+
+  /*PRODUCCTO 12 */
+
+  function mostrar12() {
+    document.getElementById("info12").innerHTML = "<h3> colageno </h3>"
+    +"<h3> codigo: 853919008021</h3>"
+    +"<h3> Peso Total: 0.25 libras (567g.) </h3>"
+    +"<h3> precio: $180.666 </h3>"
+    +"<h3> cantidad #: 16 </h3>"
+    +"<h3> presentacion: tarro 20 oz (567 g)</h3>"
+    +"<h3> Vence: 28 Enero 2026 </h3>"
+    +"<h3> Origen: EUA </h3>"
+  
+
+  
+  }
+
+
+
+
+
+  
+
+
+  
+  
+
+     
+      
+
+  
+
